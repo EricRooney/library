0c53fb3a949f0df335372e25e876956c6160c7fb
EricRooney <erooneous@gmail.com>
2020-01-27 15:51:05 -0700

    Init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace library
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      StoreController sc = new StoreController();
+      sc.Run();
+    }
+  }
+}
